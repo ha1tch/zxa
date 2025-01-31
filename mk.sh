07288c94934d9348321a8c932fe44b8f8bf44e3f
ha1tch <bitdrome@gmail.com>
2025-01-31 12:02:16 -0300

    Add Mac Intel binaries to makescript

--- mk.sh
+++ mk.sh
@@ -5,9 +5,10 @@ mkdir -p bin
 
 echo "Building all binaries for zxa..."
 pushd cmd/zxa
-GOOS=windows GOARCH=amd64 go build -x -o ../../bin/zxa.exe       main.go
-GOOS=windows GOARCH=386   go build -x -o ../../bin/zxa.win32.exe main.go
-GOOS=linux   GOARCH=amd64 go build -x -o ../../bin/zxa.linux     main.go
-GOOS=linux   GOARCH=386   go build -x -o ../../bin/zxa.linux32   main.go
-GOOS=darwin  GOARCH=arm64 go build -x -o ../../bin/zxa.mac       main.go
+GOOS=windows GOARCH=amd64 go build -x -o ../../bin/zxa.win64.exe   main.go
+GOOS=windows GOARCH=386   go build -x -o ../../bin/zxa.win32.exe   main.go
+GOOS=linux   GOARCH=amd64 go build -x -o ../../bin/zxa.linux64     main.go
+GOOS=linux   GOARCH=386   go build -x -o ../../bin/zxa.linux32     main.go
+GOOS=darwin  GOARCH=arm64 go build -x -o ../../bin/zxa.mac64.m1    main.go
+GOOS=darwin  GOARCH=amd64 go build -x -o ../../bin/zxa.mac64.intel main.go
 popd
